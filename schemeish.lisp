6859701b53a373adb9f3cef7c8021a55b66da40f
Christopher Hebert <hebert.christopherj@gmail.com>
2021-03-31 10:51:48 -0400

    added optional and keyword arguments to lambda and define

--- schemeish.lisp
+++ schemeish.lisp
@@ -87,23 +87,106 @@ Used to annotate functions that are used in macros."
 	 '(((1 2 3) (A B C)) (EXPR EXPR2) ((D E F)) (EXPR3))))
 
 (for-macros
-  (defun define-form-name (form)
-    (first (flatten form)))
+  (defun optional-arg-list->lambda-list (args)
+    (cond
+      ((consp args)
+       (let ((arg (first args)))
+	 (cond
+	   ((consp arg) `(,arg ,@(optional-arg-list->lambda-list (rest args))))
+	   (t (error "Expected optional argument ~S to be of the form (name default-value-form) or (name)." arg)))))
+      ((null args) ())
+      ((symbolp args) (error "Optional argument list cannot have a rest argument."))))
+
+  (defun keyword-arg->lambda-list-arg (arg)
+    (if (consp arg)
+	(cons (intern (symbol-name (first arg))) (rest arg))
+	(intern (symbol-name arg))))
+  
+  (defun keyword-arg-list->lambda-list (args)
+    (cond
+      ((consp args)
+       (let ((arg (first args)))
+	 (cond
+	   ((consp arg)
+	    (let ((arg-name (first arg)))
+	      (cond
+		((keywordp arg-name) `(,(keyword-arg->lambda-list-arg arg) ,@(keyword-arg-list->lambda-list (rest args))))
+		(t (error "Expected keyword argument ~S to be of the form :key or (:key) or (:key default-value-form)" arg)))))
+	   ((keywordp arg)
+	    `(,(keyword-arg->lambda-list-arg arg) ,@(keyword-arg-list->lambda-list (rest args))))
+	   (t (error "Expected keyword argument ~S to be of the form :key or (:key) or (:key default-value-form)" arg)))))
+      ((null args) ())
+      ((symbolp args) (error "Keyword argument list not compatible with rest argument."))))
+  
   (defun arg-list->lambda-list (args)
     (cond
       ((consp args)
-       (cons (first args) (arg-list->lambda-list (rest args))))
+       (let ((arg (first args)))
+	 (cond
+	   ((consp arg)
+	    (let ((arg-name (first arg)))
+	      (cond
+		((keywordp arg-name)
+		 `(&key ,(keyword-arg->lambda-list-arg arg) ,@(keyword-arg-list->lambda-list (rest args))))
+		((symbolp arg-name)
+		 `(&optional ,arg ,@(optional-arg-list->lambda-list (rest args))))
+		(t
+		 (error "bad thing to be an arg-name: ~S" arg-name)))))
+	   ((keywordp arg)
+	    `(&key ,(keyword-arg->lambda-list-arg arg) ,@(keyword-arg-list->lambda-list (rest args))))
+	   (t (cons arg (arg-list->lambda-list (rest args)))))))
       ((null args) '())
       ((symbolp args) `(&rest ,args))
-      (t (error "bad thing to be in an arglist: ~S" args))))
+      (t (error "bad thing to be in an arglist: ~S" args)))))
 
-  (defmacro λ (arg-list &body body)
-    "A lambda with scheme style arugment lists. Some examples:
-  (λ (arg1 arg2 arg3) (list arg1 arg2 arg3)) ;; Arity: 3
-  (λ (arg1 . args) (list* arg1 args)) ;; Arity: at least 1
-  (λ args args) ;; Arity: at least 0
+;; Null arguments
+(assert (null (arg-list->lambda-list ())))
+;; position + rest
+(assert (equal (arg-list->lambda-list '(arg1 arg2 . args))
+	       '(arg1 arg2 &rest args)))
+;; positional arguments
+(assert (equal (arg-list->lambda-list '(arg1 arg2 arg3))
+	       '(arg1 arg2 arg3)))
+;; optional arugments
+(assert (equal (arg-list->lambda-list '(arg1 (opt-arg "optional") (opt-arg2 "optional2") (null-arg)))
+	       '(arg1 &optional (opt-arg "optional") (opt-arg2 "optional2") (null-arg))))
+
+;; Can't mix positional and optional
+(assert (null (ignore-errors (arg-list->lambda-list '(arg1 (opt-arg "optional") bad-arg (opt-arg2 "optional2"))))))
+;; Can't mix optional and rest (...well you *could*)
+(assert (null (ignore-errors (arg-list->lambda-list '(arg1 (opt-arg "optional") . bad-args)))))
+;; Bad arg-name for optional args
+(assert (null (ignore-errors (arg-list->lambda-list '(arg1 ((oops-arg) "form"))))))
+
+(assert (equal (arg-list->lambda-list '(a1 a2 :k1 :k2))
+	       '(A1 A2 &KEY K1 K2)))
+(assert (equal (arg-list->lambda-list '(:k1))
+	       '(&key k1)))
+(assert (equal (arg-list->lambda-list '(a1 a2 (:k1 "default") :k2 (:k3 "default2")))
+	       '(A1 A2 &KEY (K1 "default") K2 (K3 "default2"))))
+(assert (null (ignore-errors (arg-list->lambda-list '(a1 a2 (:k1 "default") bad-positional)))))
+(assert (null (ignore-errors (arg-list->lambda-list '(a1 a2 (:k1 "default") (bad-optional))))))
+
+
+(defmacro λ (arg-list &body body)
+  "A lambda with scheme style arugment lists. Some examples:
+  (λ (arg1 arg2 arg3) (list arg1 arg2 arg3)) ; Arity: 3
+  (λ (arg1 . args) (list* arg1 args)) ; Arity: at least 1
+  (λ args args) ; Arity: at least 0
+
+  (λ args args) ; all arguments stored in args
+  (λ (p1 p2 . args) (list* p1 p2 args)) ; Rest arg
+  (λ (p1 p2 (o1 \"default\") (o2 \"default\")) (list p1 p2 o1 o2)) ; Optional args
+  (λ (p1 p2 :k1 (:k2 \"default\")) (list p1 p2 k1 k2)) ; keyword args
+  Rest/Optional/Keyword arguments are not compatible with each other.
+  See DEFINE for more information on argument lists.
 "
-    `(lambda ,(arg-list->lambda-list arg-list) ,@body))
+  `(lambda ,(arg-list->lambda-list arg-list) ,@body))
+
+(for-macros
+  (defun define-form-name (form)
+    (first (flatten form)))
+  
   (defun define-form->lambda-list (form)
     (assert (consp form))
     (arg-list->lambda-list (cdr form)))
@@ -245,42 +328,37 @@ Used to annotate functions that are used in macros."
     body...) ;; Expands to
   (defun function-name (arg1 arg2 &rest args)
     body...)
+
+  ;; Optional arguments are specified as (arg-name default-value-form) or (arg-name) which defaults to nil.
+  (define (fn pos1 pos2 (opt1) (opt2 \"default\"))
+    (list pos1 pos2 opt1 opt2))
+
+  ;; Keyword arguments are specified as :arg-name, (:arg-name) both of which default to nil, or (:arg-name default-value-form).
+  (define (fn pos1 pos2 (:k1 \"default\") :k2)
+    (list pos1 pos2 k1 k2))
+
+  ;; Rest/Optional/Keyword arguments are not compatible with each other.
     
   ;; Defines can be nested like in scheme.
   (define (outer-function-name oarg1 oarg2 . oargs)
-    ;; Binds both function/lexical variable
     (define (inner-function-name a1 a2) (list a1 a2))
+
+    ;; Definitions can be mutually recursive.
     (define (mutually-recursive-function1 a) (mutually-recursive-function2 a))
     (defien (mutually-recursive-function2 a) (mutually-recursive-function1 a))
-    (inner-function-name oarg1 oarg2)
-    inner-function-name)
+
+    ;; Nested defines bind both function/lexical variable
+    (inner-function-name oarg1 oarg2) ; as a function
+    inner-function-name) ; returned as a value
   
-  ;; Define nested functions.
+  ;; Define can define functions which return closures:
   (define ((left-curry . args) f)
     ...)
+  ;; Expands to
   (defun left-curry (&rest args)
     (λ (f)
        ...))"
   (expand-define name-or-form body))
-
-
-;; TODO:
-#+nil
-(define (fn-name first (:key "default") (:key2 "default2"))
-  (list first key key2))
-
-#+nil
-(defun fn-name (first &key (key "default") (key2 "default2"))
-  (list first key key2))
-
-#+nil
-(define (fn-name first (one 1) (two 2))
-  (list first one two))
-
-#+nil
-(defun fn-name (first &optional (one 1) (two 2))
-  (list first one two))
-
 
 (define (((test-nested-defines x) y . yargs) . zargs)
   "Returns a thing"
@@ -521,7 +599,7 @@ Example:
   "Remove using eq? as a test."
   (remove v list :test #'eq))
 
-(defun remove* (v-list list &optional (test #'equal?))
+(define (remove* v-list list (test #'equal?))
   "Removes all elements in v-list from list."
   (foldl (λ (v result) (remove v result :test test)) list v-list))
 
@@ -531,12 +609,14 @@ Example:
 
 (define (remq* v-list list) (remove* v-list list #'eq?))
 
-(defun sort (list less-than? &key (extract-key (λ (x) x)))
+(define (sort list less-than? (:extract-key (λ (x) x)))
   "Returns a sorted list."
   (cl:sort (copy-list list) less-than? :key extract-key))
 
-(assert (equal (let ((original-list '(1 3 4 2)))
-		 (sort original-list '<))
+(assert (equal (let* ((original-list '(1 3 4 2))
+		      (sorted-list (sort original-list '<)))
+		 (assert (equal '(1 3 4 2) original-list))
+		 sorted-list)
 	       '(1 2 3 4)))
 
 (define (memf proc list)
@@ -651,7 +731,7 @@ Example:
   "Remove nil from the result of mapping proc over lists."
   (remove nil (apply 'map proc lists)))
 
-(defun range (end &optional (start 0) (step 1))
+(define (range end (start 0) (step 1))
   "Return a list of elements from [start,end) using step as the stepsize."
   (if (<= start end)
       (loop for i from start below end by step collecting i)
@@ -1024,7 +1104,7 @@ Example:
 (assert (stream-empty? (stream-filter (λ (x) (not (<= 0.0 x 1.0)))
 				      (stream-take (random-stream 1.0) 10))))
 
-(defun alist-ref (alist key &optional failure-result)
+(define (alist-ref alist key (failure-result))
   "Rerturns the value associated with key in alist, else the failure-result."
   (let ((pair (assoc key alist :test #'equal?)))
     (if (pair? pair)
@@ -1037,7 +1117,7 @@ Example:
   "Returns an alist with key set to value."
   (acons key value (alist-remove alist key)))
 
-(defun alist-update (alist key updater &optional failure-result)
+(define (alist-update alist key updater (failure-result))
   "Applies updater to the value associated with key and updates the result in alist.
 Applies updater to failure-result if key is not present."
   (alist-set alist key [updater (alist-ref alist key failure-result)]))
