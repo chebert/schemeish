740dae3f4fb71df251aff8fbc3e90ee7ddac8c27
Christopher Hebert <hebert.christopherj@gmail.com>
2021-09-12 09:16:44 -0400

    bundle: auto-generate simple getters and setters

--- src/bundle.lisp
+++ src/bundle.lisp
@@ -35,8 +35,70 @@ when given a bundle with this type-predicate"
 (defparameter *get-bundle-permissions* (gensym))
 (defparameter *bundle?* (make-bundle-predicate :bundle))
 
-(defmacro bundle (type-predicate &rest fn-names)
+(define (get-fn-identifier? fn-identifier)
+  (and (eql (first fn-identifier) :get)
+       (symbol? (second fn-identifier))
+       (empty? (rest (rest fn-identifier)))))
+(define (set-fn-identifier? fn-identifier)
+  (and (eql (first fn-identifier) :set!)
+       (symbol? (second fn-identifier))
+       (or (empty? (rest (rest fn-identifier)))
+	   (and (symbol? (third fn-identifier))
+		(empty? (rest (rest (rest fn-identifier))))))))
+
+(define (set-fn-identifier-setter-keyword fn-identifier)
+  (make-keyword (make-symbol (or (and-let* ((name (third fn-identifier))) (symbol->string name))
+				 (concatenate 'string "SET-" (symbol->string (second fn-identifier)) "!")))))
+
+(define (fn-identifier->permission-name fn-identifier)
+  (cond ((symbol? fn-identifier) (make-keyword fn-identifier))
+	((get-fn-identifier? fn-identifier) (make-keyword (second fn-identifier)))
+	((set-fn-identifier? fn-identifier)
+	 (make-keyword (set-fn-identifier-setter-keyword fn-identifier)))))
+
+;; TODO: switch to a case statement
+(define (bundle-fn-identifier->permission-form arg-name fn-identifier)
+  (let* ((permission-name (fn-identifier->permission-name fn-identifier))
+	 (test-permission-form `(eq ,permission-name ,arg-name)))
+    (cond ((symbol? fn-identifier)
+	   `(,test-permission-form ,fn-identifier))
+	  ((get-fn-identifier? fn-identifier)
+	   `(,test-permission-form (lambda () ,(second fn-identifier))))
+	  ((set-fn-identifier? fn-identifier)
+	   (let ((value-name (unique-symbol 'value)))
+	     `(,test-permission-form
+	       (lambda (,value-name) (set! ,(second fn-identifier) ,value-name))))))))
+
+(assert (equal? (bundle-fn-identifier->permission-form 'arg 'fn-name)
+		'((EQ :FN-NAME ARG) FN-NAME)))
+(assert (equal? (bundle-fn-identifier->permission-form 'arg '(:get variable-name))
+		'((EQ :VARIABLE-NAME ARG)
+		  (LAMBDA NIL
+		    VARIABLE-NAME))))
+(assert (equal? (with-readable-symbols
+		  (bundle-fn-identifier->permission-form 'arg '(:set! variable-name)))
+		'((EQ :SET-VARIABLE-NAME! ARG)
+		  (LAMBDA (VALUE-NAME)
+		    (SET! VARIABLE-NAME VALUE)))))
+(assert (equal? (with-readable-symbols
+		  (bundle-fn-identifier->permission-form 'arg '(:set! variable-name setter-name!)))
+		'((EQ :setter-name! ARG)
+		  (LAMBDA (VALUE-NAME)
+		    (SET! VARIABLE-NAME VALUE)))))
+
+(defmacro bundle (type-predicate &rest fn-identifiers)
   "Create a bundle of permissions for closure objects.
+A bundle is a function (bundle-proc msg) => permission, where each permission
+is meant to be a locally defined function described by fn-identifiers.
+Each fn-identifier is one of:
+   fn-name => a symbolic name which maps to a function value.
+     Produces a :fn-name permission.
+   (:get variable-name) => variable-name is a symbolic name which maps to a value.
+     Produces a :variable-name permission which returns a function of zero arguments.
+   (:set! variable-name <setter-name>) => variable-name is a symbolic name which maps to a value.
+     Produces a :setter-name permission which returns a function of one argument.
+     If setter-name defaults to :set-variable-name! if not provided.
+                                         
 Type-predicate is nil or a predicate created by make-bundle-predicate.
 Example:
     (defparameter *point?* (make-bundle-predicate :point))
@@ -55,17 +117,20 @@ Example:
       [[point :set-x!] 32]
       (assert (= 32 [[point :get-x]]))
       (assert [*point?* point])
-      (bundle-permissions bundle) ; => '(:get-x :get-y :set-x! :set-y!)
-      )"
-  `(lambda (arg)
-     (cond
-       ((eq *get-bundle-type-predicate* arg)
-	,(cond
-	   ((null? type-predicate) *bundle?*)
-	   ((symbolp type-predicate) (symbol-function type-predicate))
-	   (t `(or ,type-predicate *bundle?*))))
-       ((eq *get-bundle-permissions* arg) ',(map (lambda (name) (make-keyword name)) fn-names))
-       ,@ (map (lambda (name) `((eq ,(make-keyword name) arg) ,name)) fn-names))))
+      (bundle-permissions bundle) ; => '(:get-x :get-y :set-x! :set-y!))"
+  (let* ((arg-name (unique-symbol 'arg))
+	 (permission-forms (map (lcurry #'bundle-fn-identifier->permission-form arg-name) fn-identifiers)))
+    (assert (every #'identity permission-forms))
+    `(lambda (,arg-name)
+       (cond
+	 ((eq *get-bundle-type-predicate* ,arg-name)
+	  ,(cond
+	     ((null? type-predicate) '*bundle?*)
+	     ((symbolp type-predicate) (symbol-function type-predicate))
+	     (t type-predicate)))
+	 ((eq *get-bundle-permissions* ,arg-name) ',(map #'fn-identifier->permission-name fn-identifiers))
+	 ;; TODO: switch to a case statement
+	 ,@permission-forms))))
 
 (define (bundle-documentation bundle)
   "Generates documentation for bundle and all of its permissions."
